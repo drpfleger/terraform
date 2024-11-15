970fc56b6f6678c7f203c696e691444edaa9da13
Philipp Barnau <philipp.barnau@dr-pfleger.de>
2024-11-15 13:55:15 +0100

    Refactor diagnostic settings to support user-defined categories and metrics

--- diagnostic-settings/diagnostic.tf
+++ diagnostic-settings/diagnostic.tf
@@ -6,7 +6,14 @@ resource "azurerm_monitor_diagnostic_setting" "diagnostic" {
   log_analytics_destination_type = var.destination_type == "Skip" ? null : var.destination_type
 
   dynamic "enabled_log" {
-    for_each = local.category_types == null ? [] : local.category_types
+    for_each = local.category_groups == null ? [] : local.category_groups
+    content {
+      category_group = enabled_log.value
+    }
+  }
+
+  dynamic "enabled_log" {
+    for_each = local.category_groups == null ? (local.categories == null ? [] : local.categories) : []
     content {
       category = enabled_log.value
     }
--- diagnostic-settings/meta.tf
+++ diagnostic-settings/meta.tf
@@ -3,9 +3,21 @@ data "azurerm_monitor_diagnostic_categories" "diag" {
 }
 
 locals {
-  category_groups = toset(data.azurerm_monitor_diagnostic_categories.diag.log_category_groups)
-  category_types  = toset(data.azurerm_monitor_diagnostic_categories.diag.log_category_types)
-  metrics         = toset(data.azurerm_monitor_diagnostic_categories.diag.metrics)
+  category_groups = (
+    var.user_defined_category_groups != null || var.user_defined_categories != null
+    ? toset(var.user_defined_category_groups)
+    : toset(data.azurerm_monitor_diagnostic_categories.diag.log_category_groups)
+  )
+  categories = (
+    var.user_defined_categories != null || var.user_defined_category_groups != null
+    ? toset(var.user_defined_categories)
+    : toset(data.azurerm_monitor_diagnostic_categories.diag.log_category_types)
+  )
+  metrics = (
+    var.user_defined_metrics != null
+    ? toset(var.user_defined_metrics)
+    : toset(data.azurerm_monitor_diagnostic_categories.diag.metrics)
+  )
 
   # If a resource should be created can be defined with count property that requires an number value
   set_diagnostic_settings = var.enabled == true ? 1 : 0
