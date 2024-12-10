a5e89162278c2c7c1bbda1264e187beea2daede6
philipp-barnau <162140430+philipp-barnau@users.noreply.github.com>
2024-12-10 11:25:53 +0100

    Issue/4 (#6)
    * Add variable for group_assignable_to_role. refactored locals to its own file.
    
    * Update assignable_to_role to use variable for better configurability

--- main-resource-group/entra.tf
+++ main-resource-group/entra.tf
@@ -2,5 +2,5 @@ resource "azuread_group" "admin_group" {
   display_name       = local.admin_group_name
   mail_enabled       = false
   security_enabled   = true
-  assignable_to_role = true
+  assignable_to_role = var.group_assignable_to_role
 }
