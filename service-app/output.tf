37b0e242eba77eb3144fc220bd11d8f20c7b8637
philipp-barnau <162140430+philipp-barnau@users.noreply.github.com>
2025-05-07 08:47:01 +0200

    Issue/28 (#30)
    * feat: add outputs for Entra Id application details
    
    * Update service-app/output.tf
    
    add client_id
    
    Co-authored-by: blasotta <97959572+blasotta@users.noreply.github.com>
    
    * feat: add output for service principal ID in Entra Id application
    
    ---------
    
    Co-authored-by: blasotta <97959572+blasotta@users.noreply.github.com>

--- service-app/output.tf
+++ service-app/output.tf
@@ -7,11 +7,18 @@ output "application_object_id" {
   value       = azuread_application.main.object_id
   description = "The object ID of the Entra Id application."
 }
+
 output "application_client_id" {
   value       = azuread_application.main.client_id
   description = "The client ID of the Entra Id application."
 }
+
 output "application_name" {
   value       = azuread_application.main.display_name
   description = "The display name of the Entra Id application."
+}
+
+output "service_principal_id" {
+  value       = azuread_service_principal.main.id
+  description = "The ID of the Entra Id service principal."
 }
