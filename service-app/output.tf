59d3aaa78f5a614f29ce644c0669b28d4c980b93
philipp-barnau <162140430+philipp-barnau@users.noreply.github.com>
2026-02-17 15:52:01 +0100

    Rename output variable to application_app_role_ids

--- service-app/output.tf
+++ service-app/output.tf
@@ -28,7 +28,7 @@ output "service_principal_object_id" {
   description = "The object ID of the Entra Id service principal."
 }
 
-output "app_role_ids" {
+output "application_app_role_ids" {
   value       = azuread_application.main.app_role_ids
   description = "The mapping of app role values to role IDs of this application. Useful for getting role ID by value lookup."
 }
