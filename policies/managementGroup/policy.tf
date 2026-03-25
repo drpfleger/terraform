8232308c0d3ba54825d5454a3f2f4264bc3cb4b0
Philipp Barnau <philipp.barnau@dr-pfleger.de>
2026-03-25 14:55:56 +0100

    Refactor management group policy module: update variable names for clarity, enhance README documentation, and correct management group references in policy definition and assignment.

--- policies/managementGroup/policy.tf
+++ policies/managementGroup/policy.tf
@@ -3,13 +3,15 @@ resource "azurerm_policy_definition" "main" {
   display_name        = var.policy_display_name
   mode                = var.policy_mode
   policy_type         = "Custom"
-  management_group_id = data.azurerm_management_group.main.id
+  management_group_id = data.azurerm_management_group.definition.id
   policy_rule         = jsonencode(var.policy_rule)
   metadata            = jsonencode(var.meta_data)
 }
 
-resource "azurerm_management_group_policy_assignment" "example" {
+resource "azurerm_management_group_policy_assignment" "main" {
+  for_each = data.azurerm_management_group.assignment
+
   name                 = var.assignment_name
   policy_definition_id = azurerm_policy_definition.main.id
-  management_group_id  = data.azurerm_management_group.main.id
+  management_group_id  = each.value.id
 }
